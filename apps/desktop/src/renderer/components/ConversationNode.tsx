acee1371a918567f0720d42ed3a06ed3548f2c8f
Max Prokopp <maxprokopp@hotmail.com>
2026-01-17 14:10:35 +0100

    agent node chat view displaying

--- apps/desktop/src/renderer/components/ConversationNode.tsx
+++ apps/desktop/src/renderer/components/ConversationNode.tsx
@@ -434,7 +434,16 @@ function ConversationNode({ data, id, selected }: NodeProps) {
               right: `${textSelection.position.right}px`,
             }}
           >
-            +
+            <svg width="16" height="16" viewBox="0 0 162 162" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clipPath="url(#clip0_1022_356_conv)">
+                <path d="M89.3555 152.441V8.69141C89.3555 4.00391 85.3516 0 80.5664 0C75.7812 0 71.875 4.00391 71.875 8.69141V152.441C71.875 157.129 75.7812 161.133 80.5664 161.133C85.3516 161.133 89.3555 157.129 89.3555 152.441ZM8.69141 89.2578H152.441C157.129 89.2578 161.133 85.3516 161.133 80.5664C161.133 75.7812 157.129 71.7773 152.441 71.7773H8.69141C4.00391 71.7773 0 75.7812 0 80.5664C0 85.3516 4.00391 89.2578 8.69141 89.2578Z" fill="currentColor" fillOpacity="0.85"/>
+              </g>
+              <defs>
+                <clipPath id="clip0_1022_356_conv">
+                  <rect width="161.133" height="161.23" fill="white"/>
+                </clipPath>
+              </defs>
+            </svg>
           </div>
         )}
       </div>
